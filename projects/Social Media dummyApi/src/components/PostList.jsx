c70acaf3fa6d10c3b65cfff0123e3a748c19f58b
DishuSingla01 <singladishu815@gmail.com>
2026-02-28 17:16:08 +0530

    add loading state during fetching data

--- projects/Social Media dummyApi/src/components/PostList.jsx
+++ projects/Social Media dummyApi/src/components/PostList.jsx
@@ -1,30 +1,32 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
-  const {postList, addInitialPost} = useContext(PostListData);
-  const [dataFetched, setDataFetched] = useState(false);
+  const { postList, addInitialPost } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
 
-  if (!dataFetched) {
+  useEffect(() => {
+    setFetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPost(data.posts);
+        setFetching(false);
       });
-
-    setDataFetched(true);
-  }
+  }, []);
 
   const handleGetPostClick = () => {};
 
   return (
     <>
-      {postList.length === 0 && (
+      {fetching && <LoadingSpinner/>}
+      {!fetching && postList.length === 0 && (
         <WelcomeMessage onGetPostClick={handleGetPostClick} />
       )}
-      {postList.map((post) => (
+      {!fetching && postList.map((post) => (
         <Post key={post.id} post={post}></Post>
       ))}
     </>
